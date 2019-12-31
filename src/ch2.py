2a5182d7a1d5da56bd17937a3b8453a3a76dc09d
ecssiah <ecssiah@gmail.com>
2019-12-31 12:14:07 -0800

    Remove test.py

--- src/ch2.py
+++ src/ch2.py
@@ -6,8 +6,15 @@ import matplotlib.pyplot as plt
 # Problem 8c
 
 college_data = pd.read_csv('datasets/College.csv')
+
+college_data[['Outstate', 'Private']]
+
 fig1, ax1 = plt.subplots()
 ax1.set_title('Outstate and Private')
 ax1.boxplot(college_data['Outstate'])
+
+fixed_college_data = college_data['Private'].map({'Yes': 1, 'No': 0})
+
+ax1.boxplot(fixed_college_data)
 
 # %%
