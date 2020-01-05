d32121bd85a16ff22b3225578a3a5da77826e44b
ecssiah <ecssiah@gmail.com>
2020-01-04 17:38:51 -0800

    Update seaborn_test to correctly import

--- src/seaborn_test.py
+++ src/seaborn_test.py
@@ -1,11 +1,11 @@
+# %%
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set(style="darkgrid")
-
-# %%
 
 tips = sns.load_dataset("tips")
 sns.relplot(x='total_bill', y='tip', size='size', sizes=(15, 200), data=tips)
